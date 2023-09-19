61a11ba8abc3d20f46fa766a4a27b6a13fa9fe2e
Mark Howe <mvahowe@gmail.com>
2023-09-19 12:07:15 +0200

    Add front and chapter notes

--- scripts/make_html.js
+++ scripts/make_html.js
@@ -94,6 +94,43 @@ const getBookName = (pk, docSetId, bookCode) => {
     return bookCode;
 }
 
+const maybeChapterNotes = (chapterN) => {
+    const chapterNoteRecord = notes[`${chapterN}_intro`];
+    if (chapterNoteRecord) {
+        console.log(`  Notes for Chapter ${chapterN}`);
+        const chapterNoteText = chapterNoteRecord.split("<br><br>");
+        const chapterNoteHeading = chapterNoteText[0].replace(/^# +/,"");
+        let noteParas = [];
+        for (const noteLine of chapterNoteText.slice(1)) {
+            let paraClass="note_body";
+            if (noteLine.startsWith("####")) {
+                paraClass="note_h4"
+            } else if (noteLine.startsWith("###")) {
+                paraClass="note_h3"
+            } else if (noteLine.startsWith("##")) {
+                paraClass="note_h2"
+            }
+            noteParas.push(
+                templates.markdownPara
+                    .replace("%%CLASS%%", paraClass)
+                    .replace(
+                        "%%NOTE%%",
+                        noteLine
+                            .replace(/^#+ +/,"")
+                            .replace(/\(Pour[^)]+\)/g, "")
+                            .replace(/\(\[.*?\)\)/g, "")
+                            .replace(/\*\*([^*]+)\*\*/g, (m, m1) => `<span class="b">${m1}</span>`)
+                            .replace(/\*([^*]+)\*/g, (m, m1) => `<i>${m1}</i>`)                    )
+            );
+        }
+        return templates.chapterNote
+            .replace('%%NOTETITLE%%', chapterNoteHeading)
+            .replace('%%NOTEBODY%%', noteParas.join('\n'))
+    } else {
+        return "";
+    }
+}
+
 const usage = "USAGE: node make_html.js <configPath> <bookCode> <outputPath>";
 if (process.argv.length !== 5) {
     console.log(`Wrong number of arguments!\n${usage}`);
@@ -127,6 +164,10 @@ if (config.jxl.notes && config.jxl.notes.pivot) {
         if (pivotIds.has(cells[4])) {
             notes[cells[4]] = cells[6];
         }
+        if (cells[2] === "intro") {
+            const noteKey = `${cells[1]}_${cells[2]}`;
+            notes[noteKey] = cells[6];
+        }
     }
 }
 
@@ -143,15 +184,22 @@ const bookName = getBookName(pk, config.docIdForNames, bookCode);
 const readTemplate = templateName => fse.readFileSync(path.join('src', 'templates', templateName + '.html')).toString();
 
 const templates = {};
-for (const template of ['index', 'sentence', 'greekLeft', 'transLeft', 'jxl', 'jxlRow']) {
+for (const template of ['index', 'sentence', 'greekLeft', 'transLeft', 'jxl', 'jxlRow', 'chapterNote', 'markdownPara']) {
     templates[template] = readTemplate(template);
 }
 
 console.log(`${jxlJson.length} Sentences:`);
 let sentences = [];
+let chapterN = "0";
+sentences.push(maybeChapterNotes("front"));
 for (const [sentenceN, sentenceJson] of jxlJson.entries()) {
-    console.log(`  ${sentenceN + 1}`);
     const cv = cvForSentence(sentenceJson);
+    const newChapterN = cv.split(':')[0];
+    if (chapterN !== newChapterN) {
+        sentences.push(maybeChapterNotes(newChapterN));
+        chapterN = newChapterN;
+    }
+    console.log(`  ${sentenceN + 1}`);
     let leftContent = [];
     let greekContent = null;
     for (const content of config.lhs) {
@@ -175,7 +223,7 @@ for (const [sentenceN, sentenceJson] of jxlJson.entries()) {
             const noteId = notePivot[`${sentenceN + 1}`][`${chunkN + 1}`];
             sentenceNotes.push(
                 notes[noteId]
-                    .replace(/\([^)]+\)/g, "")
+                    .replace(/\(Pour[^)]+\)/g, "")
                     .replace(/\*\*([^*]+)\*\*/g, (m, m1) => `<span class="b">${m1}</span>`)
                     .replace(/\*([^*]+)\*/g, (m, m1) => `<i>${m1}</i>`)
             );

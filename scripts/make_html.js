c396ffd70df78c03287111a9312391a697ef8b9b
Mark Howe <mvahowe@gmail.com>
2023-09-20 11:14:17 +0200

    Frontmatter; better chapter and book notes

--- scripts/make_html.js
+++ scripts/make_html.js
@@ -94,11 +94,11 @@ const getBookName = (pk, docSetId, bookCode) => {
     return bookCode;
 }
 
-const maybeChapterNotes = (chapterN) => {
+const maybeChapterNotes = (chapterN, noteType) => {
     const chapterNoteRecord = notes[`${chapterN}_intro`];
     if (chapterNoteRecord) {
         console.log(`  Notes for Chapter ${chapterN}`);
-        const chapterNoteText = chapterNoteRecord.split("<br><br>");
+        const chapterNoteText = chapterNoteRecord.split(/(<br>)+/).filter(l => l.replace('<br>', '').length > 0);
         const chapterNoteHeading = chapterNoteText[0].replace(/^# +/,"");
         let noteParas = [];
         for (const noteLine of chapterNoteText.slice(1)) {
@@ -109,6 +109,10 @@ const maybeChapterNotes = (chapterN) => {
                 paraClass="note_h3"
             } else if (noteLine.startsWith("##")) {
                 paraClass="note_h2"
+            } else if (noteLine.trim().startsWith("-")) {
+                paraClass="note_list2"
+            } else if (/^[0-9]+\./.test(noteLine.trim())) {
+                paraClass="note_list1"
             }
             noteParas.push(
                 templates.markdownPara
@@ -116,14 +120,16 @@ const maybeChapterNotes = (chapterN) => {
                     .replace(
                         "%%NOTE%%",
                         noteLine
+                            .trim()
                             .replace(/^#+ +/,"")
+                            .replace(/^-+ +/,"")
                             .replace(/\(Pour[^)]+\)/g, "")
                             .replace(/\(\[.*?\)\)/g, "")
                             .replace(/\*\*([^*]+)\*\*/g, (m, m1) => `<span class="b">${m1}</span>`)
                             .replace(/\*([^*]+)\*/g, (m, m1) => `<i>${m1}</i>`)                    )
             );
         }
-        return templates.chapterNote
+        return templates[`${noteType || 'chapter'}Note`]
             .replace('%%NOTETITLE%%', chapterNoteHeading)
             .replace('%%NOTEBODY%%', noteParas.join('\n'))
     } else {
@@ -184,14 +190,18 @@ const bookName = getBookName(pk, config.docIdForNames, bookCode);
 const readTemplate = templateName => fse.readFileSync(path.join('src', 'templates', templateName + '.html')).toString();
 
 const templates = {};
-for (const template of ['index', 'sentence', 'greekLeft', 'transLeft', 'jxl', 'jxlRow', 'chapterNote', 'markdownPara']) {
+for (const template of ['index', 'sentence', 'greekLeft', 'transLeft', 'jxl', 'jxlRow', 'chapterNote', 'bookNote', 'markdownPara']) {
     templates[template] = readTemplate(template);
 }
-
 console.log(`${jxlJson.length} Sentences:`);
 let sentences = [];
+if (config.frontMatter) {
+    console.log(`  Frontmatter`);
+    const frontMatter = fse.readFileSync(path.join('data', config.frontMatter + '.html')).toString();
+    sentences.push(frontMatter);
+}
 let chapterN = "0";
-sentences.push(maybeChapterNotes("front"));
+sentences.push(maybeChapterNotes("front", "book"));
 for (const [sentenceN, sentenceJson] of jxlJson.entries()) {
     const cv = cvForSentence(sentenceJson);
     const newChapterN = cv.split(':')[0];

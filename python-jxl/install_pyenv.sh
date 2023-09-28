88838a737af455d5bfc96e18979c5385f1062dda
danielc-n <npmdanielc-n@protonmail.com>
2023-09-28 23:44:58 +0200

    added requirements for pyenv

--- python-jxl/install_pyenv.sh
+++ python-jxl/install_pyenv.sh
@@ -3,6 +3,11 @@ index_main() {
     curl -s -S -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
 }
 
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
+libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl
+
+
 index_main
 
 export PYENV_ROOT="$HOME/.pyenv"

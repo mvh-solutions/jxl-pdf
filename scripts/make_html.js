44319ea472a643e590ee55c27324794d5680b623
danielc-n <nicolas.condomitti@xenizo.fr>
2023-11-28 13:23:08 +0100

    removing useless imports

--- scripts/make_html.js
+++ scripts/make_html.js
@@ -1,12 +1,5 @@
 const doPdf = require('../src/index');
 const PythonShell = require('python-shell').PythonShell;
-var path = require('path');
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-
-const testGroup = 'Pdf generation';
-var baseDirectory = path.join(path.normalize(__dirname+"/../static/html/output"));
 
 
 const usage = "USAGE: node make_html.js <configPath> <outputDirName> [<bookCode>]";

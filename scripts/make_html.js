4125d2151af6efeb8c55600197191eddf45ff78e
danielc-n <nicolas.condomitti@xenizo.fr>
2023-12-20 17:15:19 +0100

    Added options to the script && UPDATED README.md

--- scripts/make_html.js
+++ scripts/make_html.js
@@ -1,38 +1,43 @@
 const doPdf = require('../src/index');
-// const { program } = require('commander');
+const { program } = require('commander');
 const PythonShell = require('python-shell').PythonShell;
 
 
-const usage = "USAGE: node make_html.js <configPath> <outputDirName> [<bookCode>]";
+const usage = "USAGE: node make_html.js -c <configPath> -o <outputDirName> [-b <bookCode> -p <page format>]";
 
-// program
-//     .version("0.0.1")
-//     .option('-c, --config <path>', 'path to the config file')
+program
+    .version("0.0.1")
+    .option('-c, --config <path>', 'path to the config file')
+    .option('-o, --output <output>', 'desired output folder name')
+    .option('-b, --book <book>', '3 letters book')
+    .option('-p, --page-format <size>', 'page size format (A4, A5, executive, etc.)')
+    .option('-n, --no-python', 'flag to NOT execute the python script')
 
-// program.parse();
+program.parse();
 
-// const options = program.opts();
-// const args = program.args;
-// const la = args.length;
+const options = program.opts();
+const keysOpt = Object.keys(options);
 
-if (![4, 5, 6, 7].includes(process.argv.length)) {
-    console.log(`Wrong number of arguments!\n${usage}`);
+if (!keysOpt.includes("config") && !keysOpt.includes("output")) {
+    console.log(`You must give a config file (-c, --config) and an output file name (-o, --output)!\n${usage}`);
     process.exit(1);
 }
 
-const flag = (  
-    process.argv.indexOf('--no-python') > -1 || process.argv.indexOf('-n') > -1
-);
+const flag = !keysOpt.includes("noPython");
 
-const configPath = process.argv[2];
-const outputDirName = process.argv[3];
-const cliBookCode = /\b[A-Z\d]{1,3}\b/.test(process.argv[4]) ? process.argv[4] : null;
-const pageSize = process.argv[5];
+const configPath = options.config;
+const outputDirName = options.output;
+let cliBookCode = null;
+const pageFormat = options.pageFormat;
+
+if(options.book) {
+    cliBookCode = /\b[A-Z\d]{1,3}\b/.test(options.book) ? options.book : null;
+}
 
 doPdf({configPath, outputDirName, cliBookCode}).then(() => {
     if(!flag) {
         let pyshell = new PythonShell('cut_pdf.py', {
-            mode: 'text', scriptPath:'./python-jxl', args:["FROMNODE", outputDirName, pageSize]
+            mode: 'text', scriptPath:'./python-jxl', args:["FROMNODE", outputDirName, pageFormat]
         });
     
         pyshell.on('message', function (message) {
